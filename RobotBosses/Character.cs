06cd970a1899050eceee927e7d5aa1b74c7d5fb8
Goldenlion5648 <coboudinot@gmail.com>
2019-11-14 20:51:11 -0500

    Add project files.

---
+++ RobotBosses/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace RobotBosses
+{
+    class Character
+    {
+        Texture2D texture;
+        Rectangle rec;
+
+        public Character(Texture2D tex, Rectangle rectangle)
+        {
+            rec = rectangle;
+            texture = tex;
+        }
+
+
+    }
+}
